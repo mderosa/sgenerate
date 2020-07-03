2ae929178ccfb6afd20b76f801e6c047bfea6a8f
marco <msderosa@hotmail.com>
2020-07-02 22:56:05 -0700

    Problem: need to set a title for the first chapter

--- .dir-locals.el
+++
@@ -1,3 +0,0 @@
-((python-mode
-  (flycheck-python-flake8-executable . "/home/marco/.local/share/virtualenvs/sgenerate-zcDlkrSg/bin/python")
-  (flycheck-python-pylint-executable . "/home/marco/.local/share/virtualenvs/sgenerate-zcDlkrSg/bin/python")))
